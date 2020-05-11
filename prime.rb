e24bf323d6247ae8884edaf3fc1a4dee2b5eb479
Raveen K Sharma <raveen08@gmail.com>
2020-05-11 03:49:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,1 @@
 # Add  code here!
-def prime?(nums)
-    nums.each do |num|
-    i = 2
-      while i < num
-        if num % i == 0
-          puts false
-        else
-          puts true
-        end
-        i += 1
-      end
-   end
-end
